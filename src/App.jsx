b866348839526fd24a44d4eb71c8a177a5b563b5
shahil <shahilsha699@gmail.com>
2024-11-25 00:49:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import MapInput from "./components/MapInput";
+
+function App() {
+  return (
+    <div className="main_container">
+      <MapInput></MapInput>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mainPage/MainMapPage.jsx
@@ -0,0 +1,8 @@
+const MainMapPage = ({ WorldMap }) => {
+  return (
+    <>
+      <section className="">{WorldMap}</section>
+    </>
+  );
+};
+export default MainMapPage;
